e8c02ea6e7ce30881d51682b1f636597a7a5b462
Mezray <18051@ecam.be>
2022-12-02 11:46:34 +0100

    Advent 2022 day 2

---
+++ Day_2/.py
@@ -0,0 +1,45 @@
+#Opening and reading txt
+with open('liste.txt', 'r') as f:
+    liste = [line.rstrip() for line in f if line.strip()]
+
+    
+
+#B/Y paper
+#A/X rock
+#C/Z scissors
+
+def day21(input):
+    res = 0
+    for i in range(len(input)):
+        #Rock against Paper = Win
+        if input[i][0]=='A' and input[i][2]=='Y':
+            res+=6+2
+        #Rock against Rock = Draw
+        if input[i][0]=='A' and input[i][2]=='X':
+            res+=3+1
+        #Rock against Scissors = Loose
+        if input[i][0]=='A' and input[i][2]=='Z':
+            res+=3
+                  
+        #Paper against Paper = Draw
+        if input[i][0]=='B' and input[i][2]=='Y':
+            res+=3+2
+        #Paper against Rock = Loose 
+        if input[i][0]=='B' and input[i][2]=='X':
+            res+=1
+        #Paper against Scissors = Win
+        if input[i][0]=='B' and input[i][2]=='Z':
+            res+=6+3
+        
+        #Scissors against Paper = Loose
+        if input[i][0]=='C' and input[i][2]=='Y':
+            res+=2
+        #Scissors against Rock = Win 
+        if input[i][0]=='C' and input[i][2]=='X':
+            res+=6+1
+        #Scissors against Scissors = Draw
+        if input[i][0]=='C' and input[i][2]=='Z':
+            res+=3+3
+    print(res)
+    
+day21(liste)
