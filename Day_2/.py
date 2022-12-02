21d451a20f38948a724fe3cc92eda11bb9ae1451
Rayhan <rmezghani@gmail.com>
2022-12-02 13:20:38 +0100

    Advent 2022 Day 2

--- Day_2/.py
+++ Day_2/.py
@@ -1,5 +1,5 @@
 #Opening and reading txt
-with open('liste.txt', 'r') as f:
+with open('liste2.txt', 'r') as f:
     liste = [line.rstrip() for line in f if line.strip()]
 
 #B/Y paper
@@ -41,6 +41,39 @@ def day21(input):
             #Scissors against Scissors = Draw
             if input[i][2]=='Z':
                 res+=3+3
+    #print(res)
+
+
+def day22(input):
+    res = 0
+    for i in range(len(input)):
+        if input[i][2]=='Y':
+            if input[i][0]=='A':
+                res+=3+1
+            if input[i][0]=='B':
+                res+=3+2   
+            if input[i][0]=='C':
+                res+=3+3
+
+                  
+        if input[i][2]=='X':
+            if input[i][0]=='A':
+                res+=3
+            if input[i][0]=='B':
+                res+=1
+            if input[i][0]=='C':
+                res+=2
+        
+        if input[i][2]=='Z':
+            #Scissors against Paper = Loose
+            if input[i][0]=='A':
+                res+=6+2
+            #Scissors against Rock = Win 
+            if input[i][0]=='B':
+                res+=6+3
+            #Scissors against Scissors = Draw
+            if input[i][0]=='C':
+                res+=6+1
     print(res)
-    
-day21(liste)
+
+day22(liste)
