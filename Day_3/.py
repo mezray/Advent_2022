d2d67fb1acc1ed62f633455cb1abcaf28f559eb6
Mezray <18051@ecam.be>
2022-12-03 16:40:53 +0100

    Advent 2022 Day 3

--- Day_3/.py
+++ Day_3/.py
@@ -25,13 +25,12 @@ def day32(input):
     for lines in range(0, len(input),3):
         lines1, lines2, lines3= input[lines], input[lines+1], input[lines+2]
         for sym in lines1:
-            if sym in lines2:
-                if sym in lines3:
-                    if 'a'<=sym<='z':
-                        result+= ord(sym)-ord('a') + 1
-                        break
-                    else:
-                        result+= ord(sym)-ord('A') + 1 + 26
-                        break
+            if sym in lines2 and sym in lines3:
+                if 'a'<=sym<='z':
+                    result+= ord(sym)-ord('a') + 1
+                    break
+                else:
+                    result+= ord(sym)-ord('A') + 1 + 26
+                    break
     print(result)
 day32(liste)
