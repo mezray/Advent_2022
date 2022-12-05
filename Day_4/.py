652c48c74aee8259685ec2031569c2d528712c57
Rayhan <rmezghani@gmail.com>
2022-12-05 09:48:06 +0100

    Advent 2022 Day 4

--- Day_4/.py
+++ Day_4/.py
@@ -12,3 +12,19 @@ def test(liste):
             res+=1
     print(res)
 test(liste)
+
+def test2(liste):
+    res=0
+    for i in range(len(liste)):
+        ab,bb = liste[i][0].split("-")
+        ae,be = liste[i][1].split("-")
+        if (int(ae) <=int(ab) and int(bb) <= int(be) 
+        or int(ae) >=int(ab) and int(bb) >= int(be)
+        or int(ab) in range(int(ae),int(be))
+        or int(bb) in range(int(ae),int(be))
+        or int(ae) in range(int(ab),int(bb))
+        or int(be) in range(int(ab),int(bb))):
+            res+=1
+    print(res)
+
+test2(liste)
