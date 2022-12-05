1e325d5a988863f87d28645df6f3983029228e81
Rayhan <rmezghani@gmail.com>
2022-12-05 09:32:49 +0100

    Advent 2022 Day 4

---
+++ Day_4/.py
@@ -0,0 +1,14 @@
+#Opening and reading txt
+with open('liste.txt', 'r') as f:
+    liste = [line.rstrip().split(',') for line in f if line.strip()]
+
+
+def test(liste):
+    res=0
+    for i in range(len(liste)):
+        ab,bb = liste[i][0].split("-")
+        ae,be = liste[i][1].split("-")
+        if (int(ae) <=int(ab) and int(bb) <= int(be) or int(ae) >=int(ab) and int(bb) >= int(be)):
+            res+=1
+    print(res)
+test(liste)
