ac06c11dd3eb9159107a4d3a5440984faec4e38a
Mezray <18051@ecam.be>
2022-12-06 18:32:16 +0100

    Advent 2022 Day 5 not working

---
+++ Day_5/.py
@@ -0,0 +1,50 @@
+with open('test.txt', 'r') as f:
+    liste = [line.rstrip().split('\n') for line in f if line.strip()]
+
+
+liste1=[['Z','N'],['M','C','D'],['P']]
+liste2=[['H','C','R'], 
+        ['B','J','H','L','S','F'],
+        ['R','M','D','H','J','T','Q'], 
+        ['S','G','R','H','Z','B','J'],
+        ['R','P','F','Z','T','D','C','B'],
+        ['T','H','C','G'],
+        ['S','N','V','Z','B','P','W','L'],
+        ['R','J','Q','G','C'], 
+        ['L','D','T','R','H','P','F','S']]
+
+
+def test(liste):
+    for i in range(len(liste)):
+        toAdd=[]
+        listing,b =liste[i][0].split(' from ')
+        listingNum = listing[5:]
+        listeDepart, listeArrive = b.split(' to ')
+        print(listingNum, listeDepart, listeArrive)
+        
+        
+        toAdd = liste1[int(listeDepart)-1][-int(listingNum):]
+        print(toAdd)
+
+
+        for j in range(len(toAdd)):
+            res=toAdd[j]
+            print('avant',liste1[int(listeDepart)-1])
+            liste1[int(listeDepart)-1].remove(res)
+            print('après',liste1[int(listeDepart)-1])
+
+        toAdd.reverse()
+
+        for p in range(len(toAdd)):
+            res=toAdd[p]
+            print('avant',liste1[int(listeArrive)-1])
+            liste1[int(listeArrive)-1].append(res)
+            print('après',liste1[int(listeArrive)-1])
+            
+    for u in range(len(liste1)):
+       print(liste1[u][-1:],"\n")
+        
+test(liste)
+
+    
+
